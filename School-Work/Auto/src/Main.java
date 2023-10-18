027b81ac1f2624d10531aa0a508584efc1539375
MrMidnight7331 <mr.midnight.7331@gmail.com>
2023-10-18 20:15:41 +0200

    added auto project 4 school

---
+++ School-Work/Auto/src/Main.java
@@ -0,0 +1,49 @@
+// Auto i guess
+// By: Tieno
+
+public class Main {
+    private int kilometerstand;
+    private int tankinhalt;
+    private int verbrauch;
+    private int tankgroesse;
+
+    // Konstruktor
+    public Main(int pTankgroesse, int pVerbrauch) {
+        tankgroesse = pTankgroesse;
+        verbrauch = pVerbrauch;
+        kilometerstand = 0;
+        tankinhalt = tankgroesse;
+    }
+
+    // ---Methoden---
+
+    // get Kilometerstand
+    public int getKilometerstand() {
+        return kilometerstand;
+    }
+    // get Tankinhalt
+    public int getTankinhalt() {
+        return tankinhalt;
+    }
+
+    // Tanken
+    public void tanken(int liter) {
+        if (tankinhalt + liter <= tankgroesse) {
+            tankinhalt += liter;
+        } else {
+            System.out.println("Der Tank ist voll. Hör auf zu tanken!");
+        }
+    }
+
+    // Fahren
+    public void fahren(int km) {
+        int verbrauchtMenge = km * verbrauch;
+        if (verbrauchtMenge <= tankinhalt) {
+            kilometerstand += km;
+            tankinhalt -= verbrauchtMenge;
+            System.out.println("Du hast: " + km + " hinter dir gelegt");
+        } else {
+            System.out.println("No Treibstoff :( Tanken bidde");
+        }
+    }
+}
