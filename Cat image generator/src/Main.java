7c47c996abc08375e69c84052fdfcd94524947f7
MrMidnight7331 <mr.midnight.7331@gmail.com>
2023-10-21 19:35:46 +0200

    Updated Cat Image Generator Project

--- Cat image generator/src/Main.java
+++ Cat image generator/src/Main.java
@@ -5,81 +5,85 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import javax.swing.*;
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static String catApiUrl = "https://cataas.com/cat";
-    public static String outputFileName = "cat.jpg";
+    public static File file = null;
+    public static JFrame frame = null;
 
-    // Function to download the cat image via the cataas api
-    public static void downloadCatImage(String imageUrl, String outputFilePath) throws IOException {
-        URL catUrl = new URL(imageUrl);
-        HttpURLConnection connection = (HttpURLConnection) catUrl.openConnection();
-        connection.setRequestMethod("GET");
-
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            InputStream inputStream = connection.getInputStream();
-            FileOutputStream outputStream = new FileOutputStream(outputFilePath);
-
-            int bytesRead;
-            byte[] buffer = new byte[4096];
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-
-            outputStream.close();
-            inputStream.close();
-        } else {
-            throw new IOException("HTTP GET request failed with response code: " + responseCode);
-        }
-    }
-
-    // Main function
-    public static void main(String[] args) {
-
-        // Downloads the image
+    public static void downloadCatImage(String imageUrl) {
         try {
-            downloadCatImage(catApiUrl, outputFileName);
+            URL catUrl = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) catUrl.openConnection();
+            connection.setRequestMethod("GET");
 
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+                String timeStamp = dateFormat.format(new Date());
+                String outputFilePath = "cat_" + timeStamp + ".jpg"; // Create a new file name
+                file = new File(outputFilePath); // Update the File object
+
+                try (InputStream inputStream = connection.getInputStream();
+                     FileOutputStream outputStream = new FileOutputStream(file)) {
+                    int bytesRead;
+                    byte[] buffer = new byte[4096];
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+            } else {
+                throw new IOException("HTTP GET request failed with response code: " + responseCode);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // Displaying the image
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Display JPG Image");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            JLabel imageLabel = new JLabel();
-            ImageIcon imageIcon = new ImageIcon("cat.jpg"); // Specify the path to your JPG image file
-            imageLabel.setIcon(imageIcon);
-
-            frame.getContentPane().add(imageLabel);
-            frame.pack();
-            frame.setVisible(true);
-        });
-
-        // Save or delete the image
-        File file = new File("cat.jpg");
-        String[] buttons = { "No", "Yes" };
-        int userimp = JOptionPane.showOptionDialog(null, "Save the fetched image?", "Cute Cat Image Generator", JOptionPane.INFORMATION_MESSAGE, 1, null, buttons, buttons);
-
-        if (userimp == 0) {
-            if(file.delete()){
-                    JOptionPane.showMessageDialog(null, "Image has been deleted!", "Done!",JOptionPane.INFORMATION_MESSAGE);
-                }
-                else{
-                    JOptionPane.showMessageDialog(null,"No image to delete!", "nope!", JOptionPane.INFORMATION_MESSAGE);
-                }
-
-        } else if (userimp == 1) {
-            JOptionPane.showMessageDialog(null,"Image saved as: " + System.getProperty("user.dir") + "/" + outputFileName);
-
-        } else if (userimp == -1) {
-            System.exit(0);
-            file.delete();
-        }
-
     }
 
+    public static void displayImage() {
+        if (frame != null) {
+            frame.dispose(); // Dispose of the old frame
+        }
+
+        frame = new JFrame("Display JPG Image");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel imageLabel = new JLabel();
+        ImageIcon imageIcon = new ImageIcon(file.getAbsolutePath());
+        imageLabel.setIcon(imageIcon);
+
+        frame.getContentPane().add(imageLabel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        downloadCatImage(catApiUrl);
+        displayImage();
+
+        String[] buttons = { "No, exit please", "Yes please!" };
+        int userChoice;
+
+        while (true) {
+            userChoice = JOptionPane.showOptionDialog(null, "Fetch another & Save?", "Cute Cat Image Generator",
+                    JOptionPane.INFORMATION_MESSAGE, 1, null, buttons, buttons);
+
+            if (userChoice == 1) {
+                downloadCatImage(catApiUrl);
+                displayImage();
+            } else if (userChoice == 0 || userChoice == -1 ) {
+                if (file != null) {
+                    if (file.exists()) {
+                        if (file.delete()) {
+                            System.exit(0);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Failed to delete...", "Error: ", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
