2bcb796d120fc6baa10c10d3199ea9dc81709626
MrMidnight <112803556+MrMidnight7331@users.noreply.github.com>
2023-11-20 15:06:01 +0100

    added shapes
    succ dic

---
+++ School-Work/shapes/Haus.java
@@ -0,0 +1,56 @@
+package shapes;
+
+public class Haus {
+    private Square square;
+    private Triangle t;
+    private Square fenster;
+
+    private boolean visibility = true;
+
+    private int square_size = 150;
+    private int square_hor = 100;
+    private int square_ver = 200;
+    private String haus_col = "black";
+
+    private int tri_size_ver = 150;
+    private int tri_size_hor = 200;
+    private int tri_hor = 183;
+    private int tri_ver = 100;
+
+    private int fens_size = 50;
+    private int fens_hor = 130;
+    private int fens_ver = 230;
+    private String fens_col = "yellow";
+
+
+    public Haus() {
+        square = new Square();
+        t = new Triangle();
+        fenster = new Square();
+        
+        // Square
+        square.makeVisible();
+        square.changeColor(haus_col);
+        square.changeSize(square_size);
+        square.moveHorizontal(square_hor);
+        square.moveVertical(square_ver);
+
+        // Triangle
+        t.makeVisible();
+        t.changeColor("black");
+        t.changeSize(tri_size_ver, tri_size_hor);
+        t.moveHorizontal(tri_hor);
+        t.moveVertical(tri_ver);
+
+        // Fenster
+        fenster.makeVisible();
+        fenster.changeColor(fens_col);
+        fenster.changeSize(fens_size);
+        fenster.moveHorizontal(fens_hor);
+        fenster.moveVertical(fens_ver);
+    }
+    
+
+    // Add a setter method for fens_col
+
+}
---
+++ School-Work/shapes/Square.java
@@ -0,0 +1,105 @@
+package shapes;
+
+import java.awt.*;
+
+
+public class Square
+{
+    private int size;
+    private int xPosition;
+    private int yPosition;
+    private String color;
+    private boolean isVisible;
+
+  public Square()
+    {
+        size = 30;
+        xPosition = 60;
+        yPosition = 50;
+        color = "red";
+        isVisible = false;
+    }
+
+    public void makeVisible()
+    {
+        isVisible = true;
+        draw();
+    }
+
+    public void makeInvisible()
+    {
+        erase();
+        isVisible = false;
+    }
+    
+    public void moveRight()
+    {
+        moveHorizontal(20);
+    }
+
+    public void moveLeft()
+    {
+        moveHorizontal(-20);
+    }
+
+    public void moveUp()
+    {
+        moveVertical(-20);
+    }
+
+    public void moveDown()
+    {
+        moveVertical(20);
+    }
+
+    public void moveHorizontal(int distance)
+    {
+        erase();
+        xPosition = xPosition+distance;
+        draw();
+    }
+
+    public void moveVertical(int distance)
+    {
+        erase();
+        yPosition =yPosition+ distance;
+        draw();
+    }
+
+    public void changeSize(int newSize)
+    {
+        erase();
+        size = newSize;
+        draw();
+    }
+
+    public void changeColor(String newColor)
+    {
+        color = newColor;
+        draw();
+    }
+
+    /*
+     * Draw the square with current specifications on screen.
+     */
+    private void draw()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.draw(this, color,
+                    new Rectangle(xPosition, yPosition, size, size));
+            canvas.wait(10);
+        }
+    }
+
+    /*
+     * Erase the square on screen.
+     */
+    private void erase()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.erase(this);
+        }
+    }
+}
