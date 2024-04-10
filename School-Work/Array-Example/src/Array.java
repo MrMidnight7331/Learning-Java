ea69c96f96c28adf798bac1691b77fcc466c2c01
MrMidnight7331 <mr.midnight.7331@gmail.com>
2024-04-10 22:34:15 +0200

    Modified array example as school work inschallah

--- School-Work/Array-Example/src/Array.java
+++ School-Work/Array-Example/src/Array.java
@@ -4,12 +4,12 @@
  * @version 1.0
  */
 
-public class Array { // Defined class
+public class Array { // Define class
     int[] nums; // Creats an array called nums
 
     public Array() { // Constructur
-        nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Gives num 1 - 10 into array
-        for (int i = 0; i < nums.length; i++) { // the integer i = 0, while i is smaller than the length of num, i + 1
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Gives number 1 - 10 into array
+        for (int i = 0; i < nums.length; i++) { // the integer i is = 0, while i is smaller than the length of num, i + 1
             System.out.println(nums[i]); //print everything inside num
         }
     }
