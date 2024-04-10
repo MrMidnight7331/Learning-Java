f473b55ff03d57305986323ddd553d459f6d60d5
MrMidnight7331 <mr.midnight.7331@gmail.com>
2024-04-10 22:29:28 +0200

    Added array example as school work inschallah

---
+++ School-Work/Array-Example/src/Array.java
@@ -0,0 +1,16 @@
+/**
+ * @project Array-Example
+ * @author  Tieno
+ * @version 1.0
+ */
+
+public class Array {
+    int[] nums;
+
+    public Array() {
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
