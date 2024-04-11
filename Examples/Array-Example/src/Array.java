e6450366be4de4c17c9a0b416153e31dba79dd80
MrMidnight <112803556+MrMidnight7331@users.noreply.github.com>
2024-04-11 10:16:45 +0000

    updated

--- Examples/Array-Example/src/Array.java
+++ Examples/Array-Example/src/Array.java
@@ -1,14 +1,14 @@
 /**
  * @project Array-Example
  * @author Tieno
- * @version 1.0
+ * @version Commented Example
  */
 
 public class Array { // Define class
-    int[] nums; // Creats an array called nums
+    private int[] nums; // Creats an array called nums
 
-    public Array() { // Constructur
-        nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Gives number 1 - 10 into array
+    public      Array() { // Constructur
+        nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}; // Gives number 1 - 10 into array
         for (int i = 0; i < nums.length; i++) { // the integer i is = 0, while i is smaller than the length of num, i + 1
             System.out.println(nums[i]); //print everything inside num
         }
