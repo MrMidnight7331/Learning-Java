c4f78380b1ac922f933f13abb8240db30db4d3b6
MrMidnight <112803556+MrMidnight7331@users.noreply.github.com>
2024-04-11 11:05:20 +0000

    updated

--- School-Work/Array-Example/src/Array.java
+++ School-Work/Array-Example/src/Array.java
@@ -10,7 +10,25 @@ public class Array { // Define class
     public Array() { // Constructur
         nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Gives number 1 - 10 into array
         for (int i = 0; i < nums.length; i++) { // the integer i is = 0, while i is smaller than the length of num, i + 1
-            System.out.println(nums[i]); //print everything inside num
+            //System.out.println(nums[i]); //print everything inside num
+        }
+    }
+
+    public void countTo(int x){
+        /* 
+            x = 1 - 10
+            if x > 10 or x < 1 = error
+            print x 
+        */
+
+        if (x >= 1 && x <= 10) { // check if x is between 1 and 10 (inclusive)
+            for (int i = 0; i < x; i++){
+                System.out.println(nums[i]);
+            }
+
+
+        } else {
+            System.out.println("Error: x should be between 1 and 10."); // print error message
         }
     }
 }
--- School-Work/Array-Example/src/Main.java
+++ School-Work/Array-Example/src/Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main(String[] args){
         Array array = new Array();
+
+        array.countTo(4);
     }
 }
