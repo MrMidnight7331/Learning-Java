19058112751bbf6c4f836688b84037498d1e2aa0
MrMidnight <112803556+MrMidnight7331@users.noreply.github.com>
2024-12-05 07:57:00 +0000

    added stacks

--- School-Work/Q1/Stack Introduction/Main.java
+++ School-Work/Q1/Stack Introduction/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        StackTest stacks = new StackTest();
+
+        stacks.output();
+    
+
+        System.out.println("Nums: " + stacks.count());
+        System.out.println("Exists? " + stacks.search(3));
         
     }    
 }
--- School-Work/Q1/Stack Introduction/StackTest.java
+++ School-Work/Q1/Stack Introduction/StackTest.java
@@ -1,11 +1,73 @@
-
 public class StackTest {
+    private Stack<Integer> lulstack;
 
-    private Stack<Integer>saaas;
+    public StackTest() {
+        lulstack = new Stack<Integer>();
 
-    public StackTest(){
-        
-
+        lulstack.push(1);
+        lulstack.push(2);
+        lulstack.push(3);
+        lulstack.push(4);
+        lulstack.push(5);
+        lulstack.push(6);
+        lulstack.push(7);
+        lulstack.push(8);
+        lulstack.push(9);
+        lulstack.push(10);
     }
-    
+
+    public void output() {
+        Stack<Integer> tmp = new Stack<Integer>();
+
+        while (!lulstack.isEmpty()) {
+            Integer top = lulstack.peek();
+            System.out.println(top); 
+            tmp.push(top); 
+            lulstack.pop(); 
+        }
+
+        while (!tmp.isEmpty()) {
+            lulstack.push(tmp.peek());
+            tmp.pop();
+        }
+    }
+
+    public boolean search(Integer pNum) {
+        Stack<Integer> tmp = new Stack<Integer>();
+        boolean found = false;
+
+        while (!lulstack.isEmpty()) {
+            Integer top = lulstack.peek();
+            if (top.equals(pNum)) {
+                found = true; 
+            }
+            tmp.push(top); 
+            lulstack.pop(); 
+        }
+
+        while (!tmp.isEmpty()) {
+            lulstack.push(tmp.peek());
+            tmp.pop();
+        }
+
+        return found;
+    }
+
+    public int count() {
+        Stack<Integer> tmp = new Stack<Integer>();
+        int count = 0;
+
+        while (!lulstack.isEmpty()) {
+            tmp.push(lulstack.peek());
+            lulstack.pop();
+            count++;
+        }
+
+        while (!tmp.isEmpty()) {
+            lulstack.push(tmp.peek());
+            tmp.pop();
+        }
+
+        return count;
+    }
 }
